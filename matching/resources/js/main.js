7377c037a5c71a3278bec2532ded2a1d5367de82
Nicolas Ries <nries1@gmail.com>
2017-08-31 00:00:46 -0400

    cards rendered

--- matching/resources/js/main.js
+++ matching/resources/js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   var card = '<div class="col-sm-3 col-md-3 col-lg-3 col-xl-3 card"></div>';
-  for (var i=0; i < 4; i++) {
-    $('div.cardsContainer').html(card);
-   }
+  for (var i=0; i < 16; i++) {
+    $('div.cardsContainer').append(card);
+  };
 });
